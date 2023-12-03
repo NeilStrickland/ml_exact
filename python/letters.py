092edd48906d45a6dca5cf6165e2f079397a0ae3
Hector Leitch <hector.leitch1996@gmail.com>
2023-12-03 19:49:47 +0000

    Added work on letters for multiple words

--- python/letters.py
+++ python/letters.py
@@ -288,20 +288,13 @@ def write_word(p,q,word):
     return input_grid
 
 
-
-src_dir = os.path.dirname(os.path.realpath(__file__))
-file_path = os.path.join(src_dir, '..\\notes\\words_3x3.txt')
-words_3x3 = [l.replace('\n', '').lower() for l in open(file_path, 'r').readlines()]
-
-
-
 # As we fix grid shape we can do sums with dense layer
 
 # The permute layer changes the max pooling output from
 # 10 outputs with 11 channels to 11 outputs (one for each letter)
 # over 10 channels
 # The channels are then separately summed into 1 dense output each
-def pq_conv_model(p, q, words):
+def pq_conv_model(p, q):
     global pq_conv_model
     inputs = tf.keras.Input(shape=(p, q))
     reshape1 = tf.keras.layers.Reshape((p, q, 1))(inputs)
@@ -336,8 +329,6 @@ def pq_conv_model(p, q, words):
 
     pq_conv_model.layers[5].set_weights([w2, b1])
 
-    #for word in words:
-    #    grid = write_word(p,q,word)
 
 
 
@@ -349,7 +340,7 @@ input_grid = write_letter(input_grid,"y",3,12)
 
    
 
-pq_conv_model(p=6,q=30, words=words_3x3)
+pq_conv_model(p=6,q=30)
 
 np.shape(pq_conv_model.predict(input_grid.reshape(1,6,30),verbose=0)) #(1,1,q/3,1)
 #np.shape(pq_conv_model.layers[5].get_weights()[0]) # (11,1)
@@ -373,25 +364,16 @@ pq_conv_model.predict(input_grid.reshape(1,6,30),verbose=0)
 # or some kind of "shifter" matrix to send say [1,0, ...] to [0,1, ...] etc
 
 
-def clip0_hl(x):
-    return tf.cond(x > 0, true_fn=lambda: tf.constant([1.]), false_fn=lambda: tf.constant([0.]))
-
-
-def clip_hl(x):
-    return tf.reshape(tf.map_fn(clip0_hl, tf.reshape(x, [-1])), (x.shape[1],))
-
-
-
 # after this:
 # can use StringLookup layer with invert parameter to handle the mapping back to characters/words
-def lstm_model(p, q, words):
-    global lstm_model
+def gru_model(p, q):
+    global gru_model
+    grid_l = int(np.floor(q/3))
     inputs = tf.keras.Input(shape=(p, q))
     reshape1 = tf.keras.layers.Reshape((p, q, 1))(inputs)
     convlayer = tf.keras.layers.Conv2D(11, [3, 3], [1, 1], padding='valid', activation='relu')(reshape1)
     pooling = tf.keras.layers.MaxPooling2D(pool_size=(p-1,3), strides=(p-1,3), padding='same')(convlayer)
-    reshape2 = tf.keras.layers.Reshape((int(np.ceil(q/3)), 11))(pooling)
-    mask = tf.keras.layers.Masking()(reshape2)
+    reshape2 = tf.keras.layers.Reshape((grid_l, 11))(pooling)
     
     # Recurrent1 converts the convolution output to a sequence of integers
     recurrent1 = tf.keras.layers.SimpleRNN(1, return_sequences=True, activation='linear')(reshape2)
@@ -404,21 +386,21 @@ def lstm_model(p, q, words):
     recurrent2 = tf.keras.layers.GRU(1, activation='linear', recurrent_activation='linear', return_sequences=True)(dense2)
     
     # Dense2 and dense3 produce I(s=j), for j = {1,...,10}
-    dense3 = tf.keras.layers.Dense(30, activation='relu')(recurrent2)
-    dense4 = tf.keras.layers.Dense(10, activation='linear')(dense3)
+    dense3 = tf.keras.layers.Dense(3*grid_l, activation='relu')(recurrent2)
+    dense4 = tf.keras.layers.Dense(grid_l, activation='linear')(dense3)
 
     # Merge dense layers and compute z using 2 layer bool
     # problem atm is that if I(s=j)=0 and I(x<=0)=1 then z=2 so everything doubles instead of staying the same
     # AND can be calculated as I(s=j)*I(x>0) = relu(I(s=j)+I(x>0)-1) in one dense layer
     merge1 = tf.keras.layers.Concatenate(axis=-1)([dense2,dense4])
-    dense5 = tf.keras.layers.Dense(10, activation='relu')(merge1)
+    dense5 = tf.keras.layers.Dense(grid_l, activation='relu')(merge1)
 
     # Recurrent3 returns the original sequence with the non-zeros first
     merge2 = tf.keras.layers.Concatenate(axis=-1)([recurrent1,dense5])
-    recurrent3 = tf.keras.layers.GRU(10, activation='linear', recurrent_activation='relu', return_sequences=False, reset_after=False)(merge2)
+    recurrent3 = tf.keras.layers.GRU(grid_l, activation='linear', recurrent_activation='relu', return_sequences=False, reset_after=False)(merge2)
     
-    lstm_model = tf.keras.Model(inputs=inputs, outputs=recurrent3, name='lstm_model')
-    lstm_model.compile(loss='mean_squared_error', metrics=['accuracy'])
+    gru_model = tf.keras.Model(inputs=inputs, outputs=recurrent3, name='gru_model')
+    gru_model.compile(loss='mean_squared_error', metrics=['accuracy'])
 
     # Convolution layer
     w0 = np.array([[-1,  1, -1,  1,  1,  1,  1,  1,  1,  1, 0],
@@ -436,71 +418,71 @@ def lstm_model(p, q, words):
 
     b0 = np.array([-2.5, -3.5, -4.5, -6.5, -7.5, -4.5, -6.5, -4.5, -6.5, -4.5, 0])
 
-    lstm_model.layers[2].set_weights([2*w1, 2*b0])
+    gru_model.layers[2].set_weights([2*w1, 2*b0])
 
     # Recurrent layer 1 converts the one-hot input to integers
     w2 = np.arange(1,12).reshape(11,1)
-    w3 = np.zeros(1).reshape(1,1)
+    w3 = np.zeros((1,1))
     b1 = np.zeros(1)
-    lstm_model.layers[5].set_weights([w2, w3, b1])
+    gru_model.layers[5].set_weights([w2, w3, b1])
 
     # Dense1
-    w4 = np.ones(2).reshape(1,2)
+    w4 = np.ones((1,2))
     b2 = np.array([0,-1])
-    lstm_model.layers[6].set_weights([w4, b2])
+    gru_model.layers[6].set_weights([w4, b2])
 
     # Dense2
     w5 = np.array([[1],[-1]])
     b3 = np.zeros(1)
-    lstm_model.layers[7].set_weights([w5, b3])
+    gru_model.layers[7].set_weights([w5, b3])
 
     # Recurrent layer 2 increments by 1 at each unmasked value
     w6 = np.array([[-1,0,0]]) #z_t = I(x=<0) = 1 - I(x>0)
     w7 = np.array([[0,0,1]]) #h^t = h_t-1 + 1
     b4 = np.array([[1,1,1],[0,0,0]]) #r_t = 1, h^t = h_t-1 + 1
-    lstm_model.layers[8].set_weights([w6, w7, b4])
+    gru_model.layers[8].set_weights([w6, w7, b4])
 
     #dense3
     # need 1+s-j, s-j, s-1-j
     # have as (1+s-1, s-1, s-1-1, 1+s-2, s-2, s-1-2,...)
-    w8 = np.ones(30).reshape(1,30)
-    b5 = np.repeat(-np.arange(10),3)
-    b5[1::3] = -np.arange(10)-1
-    b5[2::3] = -np.arange(10)-2
-    lstm_model.layers[9].set_weights([w8, b5])
+    w8 = np.ones((1,3*grid_l))
+    b5 = np.repeat(-np.arange(grid_l),3)
+    b5[1::3] = -np.arange(grid_l)-1
+    b5[2::3] = -np.arange(grid_l)-2
+    gru_model.layers[9].set_weights([w8, b5])
 
     #dense4
     # need to calculate this before adding bias in the following GRU layer
-    w9 = np.zeros((30,10))
-    for j in range(10):
+    w9 = np.zeros((3*grid_l,grid_l))
+    for j in range(grid_l):
         w9[(j*3):((j+1)*3),j] = np.array([1,-2,1])
-    b6 = np.zeros(10)
-    lstm_model.layers[10].set_weights([w9, b6])
+    b6 = np.zeros(grid_l)
+    gru_model.layers[10].set_weights([w9, b6])
 
     #dense5
     # calculate I(s=j and x>0)
-    w10 = np.zeros((11,10))
-    w10[0,:] = np.ones(10)
-    w10[1:11,:] = np.eye(10)
-    b7 = -np.ones(10)
-    lstm_model.layers[12].set_weights([w10, b7])
+    w10 = np.zeros((1+grid_l,grid_l))
+    w10[0,:] = np.ones(grid_l)
+    w10[1:(1+grid_l),:] = np.eye(grid_l)
+    b7 = -np.ones(grid_l)
+    gru_model.layers[12].set_weights([w10, b7])
 
     #recurrent3
     # layer receives:
     # x_t
     # I(x<=0)
     # 10 parts for I(s=j)
-    w11 = np.zeros((11,30))
+    w11 = np.zeros((1+grid_l, 3*grid_l))
     # h_t = x_t
-    w11[0,20:30] = np.ones(10)
+    w11[0,(2*grid_l):(3*grid_l)] = np.ones(grid_l)
     # z_t = 1 - I(s=j and x>0)
-    w11[1:11,0:10] = -np.eye(10)
+    w11[1:(1+grid_l),0:grid_l] = -np.eye(grid_l)
 
-    w12 = np.zeros((10,30))
+    w12 = np.zeros((grid_l,3*grid_l))
 
-    b8 = np.zeros(30)
-    b8[0:10] = np.ones(10)
-    lstm_model.layers[14].set_weights([w11, w12, b8])
+    b8 = np.zeros(3*grid_l)
+    b8[0:grid_l] = np.ones(grid_l)
+    gru_model.layers[14].set_weights([w11, w12, b8])
 
 
     # The GRU layer has one unit for each position
@@ -518,41 +500,210 @@ def lstm_model(p, q, words):
     # (2, 30) = (input shape , units x3)
 
 
+zero_grid = np.zeros([6,20])
+input_grid = write_letter(zero_grid,"c",0,0)
+input_grid = write_letter(input_grid,"i",1,4)
+input_grid = write_letter(input_grid,"t",2,8)
+input_grid = write_letter(input_grid,"x",3,17)
+
+   
+
+gru_model(p=6,q=20)
+
+#np.shape(lstm_model.predict(input_grid.reshape(1,6,30),verbose=0)) #(1,1,q/3,11)
+#np.shape(lstm_model.layers[5].get_weights()[0]) # (11,1)
+#np.shape(lstm_model.layers[5].get_weights()[1]) # (1,)
+gru_model.predict(input_grid.reshape(1,6,20),verbose=0)
+
+
+# from here we can convert the sequence into an integer and use a
+# string lookup layer to complete the word detection
+
+# to convert the sequence use a dense layer with 1 output and weights 11^j
+
+def make_vocab(file):
+    src_dir = os.path.dirname(os.path.realpath(__file__))
+    file_path = os.path.join(src_dir, file)
+    words_3x3 = [l.replace('\n', '').lower() for l in open(file_path, 'r').readlines()]
+    letters = ["i","y","j","c","o","l","h","t","u","x"]
+    layer1 = tf.keras.layers.StringLookup(vocabulary=letters)
+    numbers_3x3 = []
+    for word in words_3x3:
+        digits = layer1(list(word)).numpy()
+        word_sum = sum(digits*np.array([11]*len(digits))**np.arange(len(digits)))
+        numbers_3x3.append(word_sum)
+    return words_3x3, numbers_3x3
+
+#words_3x3, numbers_3x3 = make_vocab(file='..\\notes\\words_3x3.txt')
+
+def full_model(p, q):
+    global full_model
+    grid_l = int(np.floor(q/3))
+    inputs = tf.keras.Input(shape=(p, q))
+    reshape1 = tf.keras.layers.Reshape((p, q, 1))(inputs)
+    convlayer = tf.keras.layers.Conv2D(11, [3, 3], [1, 1], padding='valid', activation='relu')(reshape1)
+    pooling = tf.keras.layers.MaxPooling2D(pool_size=(p-1,3), strides=(p-1,3), padding='same')(convlayer)
+    reshape2 = tf.keras.layers.Reshape((grid_l, 11))(pooling)
+    recurrent1 = tf.keras.layers.SimpleRNN(1, return_sequences=True, activation='linear')(reshape2)
+    dense1 = tf.keras.layers.Dense(2, activation='relu')(recurrent1)
+    dense2 = tf.keras.layers.Dense(1, activation='linear')(dense1)
+    recurrent2 = tf.keras.layers.GRU(1, activation='linear', recurrent_activation='linear', return_sequences=True)(dense2)
+    dense3 = tf.keras.layers.Dense(3*grid_l, activation='relu')(recurrent2)
+    dense4 = tf.keras.layers.Dense(grid_l, activation='linear')(dense3)
+    merge1 = tf.keras.layers.Concatenate(axis=-1)([dense2,dense4])
+    dense5 = tf.keras.layers.Dense(grid_l, activation='relu')(merge1)
+    merge2 = tf.keras.layers.Concatenate(axis=-1)([recurrent1,dense5])
+    recurrent3 = tf.keras.layers.GRU(grid_l, activation='linear', recurrent_activation='relu', return_sequences=False, reset_after=False)(merge2)
+
+    # Convert the GRU output to a single number and then to a word using lookup layers   
+    words_3x3, numbers_3x3 = make_vocab(file='..\\notes\\words_3x3.txt')
+    dense6 = tf.keras.layers.Dense(1, activation='linear')(recurrent3)
+    lookup1 = tf.keras.layers.IntegerLookup(vocabulary=numbers_3x3)(dense6)
+    lookup2 = tf.keras.layers.StringLookup(vocabulary=words_3x3, invert=True)(lookup1)
+
+    full_model = tf.keras.Model(inputs=inputs, outputs=lookup2, name='full_model')
+    full_model.compile(loss='mean_squared_error', metrics=['accuracy'])
+
+    # Use layers from GRU model up to recurrent3
+    w0 = np.array([[-1,  1, -1,  1,  1,  1,  1,  1,  1,  1, 0],
+                   [ 1, -1, -1,  1,  1, -1, -1,  1, -1, -1, 0],
+                   [-1,  1,  1,  1,  1, -1,  1,  1,  1,  1, 0],
+                   [-1, -1, -1,  1,  1,  1,  1, -1,  1, -1, 0],
+                   [ 1,  1, -1, -1, -1, -1,  1,  1, -1,  1, 0],
+                   [-1, -1,  1, -1,  1, -1,  1, -1,  1, -1, 0],
+                   [-1, -1,  1,  1,  1,  1,  1, -1,  1,  1, 0],
+                   [ 1,  1,  1,  1,  1,  1, -1,  1,  1, -1, 0],
+                   [-1, -1,  1,  1,  1,  1,  1, -1,  1,  1, 0]])
+    w1 = w0.reshape(3,3,1,11)
+    b0 = np.array([-2.5, -3.5, -4.5, -6.5, -7.5, -4.5, -6.5, -4.5, -6.5, -4.5, 0])
+    full_model.layers[2].set_weights([2*w1, 2*b0])
+
+    w2 = np.arange(1,12).reshape(11,1)
+    w3 = np.zeros((1,1))
+    b1 = np.zeros(1)
+    full_model.layers[5].set_weights([w2, w3, b1])
+
+    w4 = np.ones((1,2))
+    b2 = np.array([0,-1])
+    full_model.layers[6].set_weights([w4, b2])
+
+    w5 = np.array([[1],[-1]])
+    b3 = np.zeros(1)
+    full_model.layers[7].set_weights([w5, b3])
+
+    w6 = np.array([[-1,0,0]])
+    w7 = np.array([[0,0,1]])
+    b4 = np.array([[1,1,1],[0,0,0]])
+    full_model.layers[8].set_weights([w6, w7, b4])
+
+    w8 = np.ones((1,3*grid_l))
+    b5 = np.repeat(-np.arange(grid_l),3)
+    b5[1::3] = -np.arange(grid_l)-1
+    b5[2::3] = -np.arange(grid_l)-2
+    full_model.layers[9].set_weights([w8, b5])
+
+    w9 = np.zeros((3*grid_l,grid_l))
+    for j in range(grid_l):
+        w9[(j*3):((j+1)*3),j] = np.array([1,-2,1])
+    b6 = np.zeros(grid_l)
+    full_model.layers[10].set_weights([w9, b6])
+
+    w10 = np.zeros((1+grid_l,grid_l))
+    w10[0,:] = np.ones(grid_l)
+    w10[1:(1+grid_l),:] = np.eye(grid_l)
+    b7 = -np.ones(grid_l)
+    full_model.layers[12].set_weights([w10, b7])
+
+    w11 = np.zeros((1+grid_l, 3*grid_l))
+    w11[0,(2*grid_l):(3*grid_l)] = np.ones(grid_l)
+    w11[1:(1+grid_l),0:grid_l] = -np.eye(grid_l)
+    w12 = np.zeros((grid_l,3*grid_l))
+    b8 = np.zeros(3*grid_l)
+    b8[0:grid_l] = np.ones(grid_l)
+    full_model.layers[14].set_weights([w11, w12, b8])
+
+    w13 = np.array([11]*grid_l)**np.arange(grid_l)
+    w13 = w13.reshape(grid_l, 1)
+    b9 = np.zeros(1)
+    full_model.layers[15].set_weights([w13, b9])
+
+
+
+zero_grid = np.zeros([6,20])
+input_grid = write_letter(zero_grid,"y",0,0)
+input_grid = write_letter(input_grid,"o",1,4)
+input_grid = write_letter(input_grid,"u",2,8)
+input_grid = write_letter(input_grid,"t",0,13)
+input_grid = write_letter(input_grid,"h",3,17)
+
+
+full_model(p=6,q=20)
+
+full_model.predict(input_grid.reshape(1,6,20),verbose=0)
+
+y = full_model.predict(input_grid.reshape(1,6,20),verbose=0)
+[z.decode() for z in y.reshape(1,)]
 
 
 
 
+# Try to implement again with a grid that may contain multiple words
+def pq_conv_model(p, q, w):
+    global pq_conv_model
+    grid_p = int(np.floor(p/w))
+    grid_q = int(np.floor(q/3))
+    inputs = tf.keras.Input(shape=(p, q))
+    reshape1 = tf.keras.layers.Reshape((p, q, 1))(inputs)
+    convlayer = tf.keras.layers.Conv2D(11, [3, 3], [1, 1], padding='valid', activation='relu')(reshape1)
+    pooling = tf.keras.layers.MaxPooling2D(pool_size=(grid_p,3), strides=(grid_p,3), padding='same')(convlayer)
+    reshape2 = tf.keras.layers.Permute((1,3,2))(pooling)
+    sum = tf.keras.layers.Dense(1, activation='linear')(reshape2)
+    pq_conv_model = tf.keras.Model(inputs=inputs, outputs=sum, name='pq_conv_model')
+    pq_conv_model.compile(loss='categorical_crossentropy', metrics=['accuracy'])
 
-zero_grid = np.zeros([6,30])
+    # Convolution layer
+    w0 = np.array([[-1,  1, -1,  1,  1,  1,  1,  1,  1,  1, 0],
+                   [ 1, -1, -1,  1,  1, -1, -1,  1, -1, -1, 0],
+                   [-1,  1,  1,  1,  1, -1,  1,  1,  1,  1, 0],
+                   [-1, -1, -1,  1,  1,  1,  1, -1,  1, -1, 0],
+                   [ 1,  1, -1, -1, -1, -1,  1,  1, -1,  1, 0],
+                   [-1, -1,  1, -1,  1, -1,  1, -1,  1, -1, 0],
+                   [-1, -1,  1,  1,  1,  1,  1, -1,  1,  1, 0],
+                   [ 1,  1,  1,  1,  1,  1, -1,  1,  1, -1, 0],
+                   [-1, -1,  1,  1,  1,  1,  1, -1,  1,  1, 0]])
+    
+    # Turns out we can just reshape w0 from the first model
+    w1 = w0.reshape(3,3,1,11)
+
+    b0 = np.array([-2.5, -3.5, -4.5, -6.5, -7.5, -4.5, -6.5, -4.5, -6.5, -4.5, 0])
+
+    pq_conv_model.layers[2].set_weights([2*w1, 2*b0])
+
+    w2 = np.ones([grid_p,1])
+
+    b1 = np.zeros(1)
+
+    pq_conv_model.layers[5].set_weights([w2, b1])
+
+
+zero_grid = np.zeros([12,18])
 input_grid = write_letter(zero_grid,"c",0,0)
 input_grid = write_letter(input_grid,"i",1,4)
 input_grid = write_letter(input_grid,"t",2,8)
 input_grid = write_letter(input_grid,"y",3,12)
 
+input_grid = write_letter(zero_grid,"j",6,0)
+input_grid = write_letter(input_grid,"u",5,4)
+input_grid = write_letter(input_grid,"l",7,8)
+input_grid = write_letter(input_grid,"y",9,12)
+
    
 
-lstm_model(p=6,q=30, words=words_3x3)
+pq_conv_model(p=12,q=18,w=2)
 
-#np.shape(lstm_model.predict(input_grid.reshape(1,6,30),verbose=0)) #(1,1,q/3,11)
-#np.shape(lstm_model.layers[5].get_weights()[0]) # (11,1)
-#np.shape(lstm_model.layers[5].get_weights()[1]) # (1,)
-lstm_model.predict(input_grid.reshape(1,6,30),verbose=0)
+np.shape(pq_conv_model.predict(input_grid.reshape(1,12,18),verbose=0)) #(1,1,q/3,1)
+#np.shape(pq_conv_model.layers[5].get_weights()[0]) # (11,1)
+#np.shape(pq_conv_model.layers[5].get_weights()[1]) # (1,)
+pq_conv_model.predict(input_grid.reshape(1,12,18),verbose=0)
 
-# idea: RNN with an extra unit
-# the extra unit takes the input and outputs the value e.g. [0,1,0] -> 2
-# second ignores the input and shifts the state one position each time
-# e.g. [1,0,0,0] -> [0,1,0,0]
-
-# then another RNN layer can take these as inputs and do
-# y_t = y_{t-1} + 2*[1,0,0,0]
-
-#vocab = ["i", "y", "j", "c", "o", "l", "h", "t", "u", "x"]
-#data = tf.constant([[2,2,0,2,4,0,0,0,0]])
-#layer = tf.keras.layers.StringLookup(vocabulary=vocab, invert=True)
-#layer(data)
-
-# GRU h_t = z_t o h_{t-1} + (1-z_t) o h^_{t-1} - other way round to wikipedia
-# see rnn.py
-# main idea of GRU in rnn.py is to use r = x
-# we could do similar r = x or z = x here
-
+# currently writes count for each letter per word separately
